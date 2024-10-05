6824b3e5e3640b3f538abc4e7ea0597df1f4caa3
brandonyeu <brandonyeu8@gmail.com>
2024-10-05 17:23:41 -0400

    Background Auth done

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
 import {NavBar} from "@/components/ours/Navbar";
 import StartPage from "@/components/ours/Start Page/StartPage";
 import TextScroll from "@/components/ours/Start Page/TextScroll/page";
+import Auth from "@/Auth";
+import AuthWrapper from "@/contexts/AuthWrapper";
+
 export default function Home() {
   return (
-    <div>
-        <NavBar/>
-        <StartPage/>
-        <TextScroll/>
-    </div>
+      <AuthWrapper>
+          <div>
+              <NavBar/>
+              <StartPage/>
+              <TextScroll/>
+              <Auth/>
+          </div>
+      </AuthWrapper>
+
   );
 }
