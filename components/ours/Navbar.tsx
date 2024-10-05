cc384fef21911deee8c3925bbab7ffe185e0eee3
chrislam365 <cllam@wpi.edu>
2024-10-05 12:17:12 -0400

    navbar

---
+++ components/ours/Navbar.tsx
@@ -0,0 +1,76 @@
+"use client";
+import { useEffect, useState } from "react";
+import { motion, useAnimation } from "framer-motion";
+import Link from "next/link";
+import Image from "next/image";
+import LogInButton from "./LogInButton";
+import SignUpButton from "./SignUpButton";
+
+
+export const NavBar = () => {
+    const [lastScrollY, setLastScrollY] = useState(0);
+    const [scrollingUp, setScrollingUp] = useState(true);
+    const controls = useAnimation();
+
+    const handleScroll = () => {
+        const currentScrollY = window.scrollY;
+
+        if (currentScrollY > lastScrollY && currentScrollY > 50) { // Add a threshold of 50px
+            setScrollingUp(false);
+        } else if (currentScrollY < lastScrollY) {
+            setScrollingUp(true);
+        }
+
+        setLastScrollY(currentScrollY);
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [lastScrollY]);
+
+    useEffect(() => {
+        if (scrollingUp) {
+            controls.start({y: 0}).then();
+        } else {
+            controls.start({y: "-100%"}).then();
+        }
+    }, [scrollingUp, controls]);
+
+    return (
+        <motion.nav
+            initial={{ y: 0 }}
+            animate={controls}
+            transition={{ type: "tween", duration: 0.3 }}
+            className="bg-white p-6 fixed w-full top-0 z-50"
+        >
+            <div className="text-black justify-between flex items-center m-auto">
+                <Link href={"/"} className={"flex gap-x-2.5 items-center"}>
+                    <Image src={"/wildscape_logo_green.png"} alt={"Logo"} width={40} height={40}/>
+
+                    {/*TODO if logged in hide this component*/}
+                    <h1 className="text-[#a7db42] text-xl">Wildscape</h1>
+                </Link>
+
+
+                <div className="flex gap-x-3 items-center">
+                    <Link href={"/#about"}>
+                        <div className="cursor-pointer mr-6">
+                            <h2 className="relative group text-md font-semibold">
+                                About
+                                <span
+                                    className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-black"></span>
+                            </h2>
+                        </div>
+                    </Link>
+                    <LogInButton/>
+                    <SignUpButton/>
+
+                </div>
+            </div>
+        </motion.nav>
+    );
+};
---
+++ components/ours/SignUpButton.tsx
@@ -0,0 +1,50 @@
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "../ui/dialog"
+import Image from "next/image";
+import {Input} from "@/components/ui/input";
+
+
+export default function SignUpButton() {
+    return(
+        <div>
+            <Dialog>
+                <DialogTrigger className={"text-md text-black font-semibold bg-[#EEEEEE] hover:bg-[#DCDCDC] transition p-3 px-4 rounded-full"}>
+                    Sign Up
+                </DialogTrigger>
+                <DialogContent>
+                    <DialogHeader className={"justify-center m-auto"}>
+                        <Image src={"/wildscape_logo_green.png"} alt={"logo"} width={50} height={50} className={"m-auto mb-4"}/>
+                        <DialogTitle className={"font-medium text-2xl"}>Welcome to Wildscape</DialogTitle>
+                        <p className={"m-auto"}>Discover. Share. Protect.</p>
+                    </DialogHeader>
+                    <DialogDescription className={""}>
+                        <h2 className={"mb-1"}>Email</h2>
+                        <Input placeholder={"Email"} className={""}/>
+                        <br/>
+                        <h2 className={"mb-1"}>Username</h2>
+                        <Input placeholder={"Username"} className={""}/>
+                        <br/>
+                        <h2 className={"mb-1"}>Password</h2>
+                        <Input placeholder={"Password"} className={""}/>
+                        <br/>
+                        <h2 className={"mb-1"}>Confirm Password</h2>
+                        <Input placeholder={"Confirm Password"} className={"p-4"}/>
+                    </DialogDescription>
+                    <DialogFooter>
+                        <button
+                            className={"text-white font-semibold bg-[#A7DB42] hover:bg-[#689917] transition p-3 px-6 mt-4  rounded-md w-full"}>
+                            Log in
+                        </button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+        </div>
+    )
+}
