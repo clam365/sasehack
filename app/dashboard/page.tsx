6b015a1f884c13d65ea29da9c76af0d364a7f868
Christopher Lam <127339027+clam365@users.noreply.github.com>
2024-10-05 20:00:03 -0400

    PhotoCard placeholder

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,23 @@
 import {DashboardNavBar} from "@/app/dashboard/components/DashboardNavbar";
+import PhotoCard from "@/components/ours/PhotoCard";
 
 export default function Page() {
     return (
         <div>
             <DashboardNavBar/>
+            <section className={"mt-28 px-10 "}>
+                <div className={"grid grid-cols-5 gap-y-5 gap-x-4"}>
+                    <PhotoCard/>
+                    <PhotoCard/>
+                    <PhotoCard/>
+                    <PhotoCard/>
+                    <PhotoCard/>
+                    <PhotoCard/>
+                    <PhotoCard/>
+
+                </div>
+
+            </section>
         </div>
     )
 }
---
+++ components/ours/PhotoCard.tsx
@@ -0,0 +1,10 @@
+
+const PhotoCard = () => {
+  return(
+      <div>
+            <img src={"/aurora.jpg"} alt={"Photo Card"} width={250} height={175} className={"rounded-xl"}/>
+      </div>
+  )
+}
+
+export default PhotoCard;
