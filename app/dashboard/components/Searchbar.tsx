46c4c23e95e9da2ab3fe69e771c1e748e1d74278
chrislam365 <cllam@wpi.edu>
2024-10-06 13:29:41 -0400

    create page done

--- app/dashboard/components/Searchbar.tsx
+++ app/dashboard/components/Searchbar.tsx
@@ -2,7 +2,7 @@ import { Search } from 'lucide-react';
 
 const SearchBar = () => {
     return (
-        <div className="flex items-center bg-gray-200 rounded-full px-4 py-3 w-full max-w-xl hover:bg-gray-300 transition">
+        <div className="flex items-center bg-[#f1f1f1] rounded-full px-4 py-3 w-full max-w-xl hover:bg-gray-200 transition">
             <Search className="text-gray-500 w-5 h-5" />
             <input
                 type="text"
