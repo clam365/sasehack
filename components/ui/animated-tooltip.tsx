48084633983e177bb0f2d2c451e5744f045f9cb2
jeremymkurtz <kurtzjeremy126@gmail.com>
2024-10-05 23:32:37 -0400

    set event not to any

--- components/ui/animated-tooltip.tsx
+++ components/ui/animated-tooltip.tsx
@@ -32,8 +32,8 @@ export const AnimatedTooltip = ({
         useTransform(x, [-100, 100], [-50, 50]),
         springConfig
     );
-    const handleMouseMove = (event: any) => {
-        const halfWidth = event.target.offsetWidth / 2;
+    const handleMouseMove = (event: React.MouseEvent<HTMLImageElement>) => {
+        const halfWidth = event.currentTarget.offsetWidth / 2;
         x.set(event.nativeEvent.offsetX - halfWidth); // set the x value, which is then used in transform and rotate
     };
 
