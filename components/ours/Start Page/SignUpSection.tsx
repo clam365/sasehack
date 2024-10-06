f7572a2d1b79bf0fdb971f217a6cbdba81a46c41
brandonyeu <brandonyeu8@gmail.com>
2024-10-05 21:27:34 -0400

    SignUpSection with Google

--- components/ours/Start Page/SignUpSection.tsx
+++ components/ours/Start Page/SignUpSection.tsx
@@ -89,14 +89,14 @@ const SignUpSection = () => {
                 {profileImage && (
                     <p className="text-sm text-green-600 mt-2">Image selected: {profileImage.name}</p>
                 )}
-                <div>
+                <div className={"flex space-x-4 justify-center"}>
                     <button
-                        className="mt-4 px-4 py-3 bg-[#a7db42] font-semibold text-white rounded-lg hover:bg-[#689917] transition"
+                        className={"mt-4 px-4 py-3 bg-[#a7db42] font-semibold text-white rounded-lg hover:bg-[#689917] transition"}
                         onClick={handleSignUp}>
                         Sign up
                     </button>
                     <button
-                        className={"flex justify-center text-white font-semibold bg-[#4484f3] hover:bg-[#3160b0] transition py-3 pl-1 pr-2 mt-4 rounded-md w-full items-center"}
+                        className={"flex justify-center mt-4 px-3 py-3 bg-[#4484f3] font-semibold text-white rounded-lg hover:bg-[#3160b0] transition items-center"}
                         onClick={handleGoogleSignUp}>
                         <FcGoogle className={"size-8 justify-self-center bg-white rounded-md mr-1"}/>
                         <p>Sign up with Google</p>
