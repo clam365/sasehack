3ad22577c8f785005c4c6adf61ee0b7181731e4e
Arnav Mishra <49102795+arnavm21@users.noreply.github.com>
2024-10-05 22:08:50 -0400

    navigation

--- components/ours/SignUpButton.tsx
+++ components/ours/SignUpButton.tsx
@@ -12,6 +12,7 @@ import { Input } from "@/components/ui/input";
 import pb from "../../lib/pocketbase";
 import { useState } from "react";
 import { FcGoogle } from 'react-icons/fc';
+import router from "next/router";
 
 
 export default function SignUpButton() {
@@ -30,7 +31,9 @@ export default function SignUpButton() {
             "passwordConfirm": passwordConfirmInput
         };
         const record = await pb.collection('users').create(data);
-        console.log(record);
+        if (record) {
+            router.push('/dashboard');
+        }
     }
 
     const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -45,7 +48,9 @@ export default function SignUpButton() {
             const authData = await pb.collection('users').authWithOAuth2({
                 provider: 'google',
             });
-            console.log('Google OAuth:', authData);
+            if (authData) {
+                router.push('/dashboard');
+            }
         } catch (error) {
             console.error('Google OAuth login failed:', error);
         }
